5ad32c7eaa8fbdc9323398524601968b37292f1b
Joydip <joydip.mondal@gmail.com>
2022-10-18 17:47:25 +0530

    Update index.js

--- codes/day-7/js-array/index.js
+++ codes/day-7/js-array/index.js
@@ -124,3 +124,7 @@ for (var indexNo in newArr) {
     console.log(indexNo + ':' + newArr[indexNo])
 }
 
+var numbers = [1, 3, 2, 6, 0, 7, 9, 4, 8, 5]
+//numbers = [0,1,2,3,4,5,6,7,8,9]
+
+//[0,2,4,6,8]
